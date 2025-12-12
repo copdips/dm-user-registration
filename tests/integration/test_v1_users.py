45deed6cb6072a71ce24b4823dbd8b7a17791492
Xiang ZHU <xiang.zhu@outlook.com>
2025-12-12 17:58:39 +0100

    add integration tests for /v1/users/resend-code and /v1/users/activate

--- tests/integration/test_v1_users.py
+++ tests/integration/test_v1_users.py
@@ -7,7 +7,8 @@ from app.domain import VerificationCode
 
 TEST_EMAIL = "test@example.com"
 TEST_PASSWORD = "toto1234"  # noqa: S105 Possible hardcoded password
-TEST_VERIFICATION_CODE = "1234"
+TEST_VERIFICATION_CODE_OLD = "1234"
+TEST_VERIFICATION_CODE_NEW = "5678"
 
 
 async def test_register_user_success(
@@ -16,7 +17,7 @@ async def test_register_user_success(
 ) -> None:
     monkeypatch.setattr(
         "app.domain.VerificationCode.generate",
-        Mock(return_value=VerificationCode(TEST_VERIFICATION_CODE)),
+        Mock(return_value=VerificationCode(TEST_VERIFICATION_CODE_OLD)),
     )
     payload = {
         "email": TEST_EMAIL,
@@ -26,3 +27,49 @@ async def test_register_user_success(
     assert response.status_code == status.HTTP_201_CREATED
     data = response.json()
     assert data["email"] == TEST_EMAIL
+
+
+async def test_resend_verification_code_success(
+    async_client: AsyncClient,
+    monkeypatch,
+) -> None:
+    monkeypatch.setattr(
+        "app.domain.VerificationCode.generate",
+        Mock(return_value=VerificationCode(TEST_VERIFICATION_CODE_NEW)),
+    )
+    payload = {
+        "email": TEST_EMAIL,
+        "password": TEST_PASSWORD,
+    }
+    response = await async_client.post("/v1/users/resend-code", json=payload)
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert "New verification code has been sent" in data["message"]
+
+
+async def test_activate_user_failed_with_old_code(
+    async_client: AsyncClient,
+) -> None:
+    payload = {
+        "email": TEST_EMAIL,
+        "password": TEST_PASSWORD,
+        "code": TEST_VERIFICATION_CODE_OLD,
+    }
+    response = await async_client.post("/v1/users/activate", json=payload)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    data = response.json()
+    assert f"Verification code is invalid for user: {TEST_EMAIL}" in data["message"]
+
+
+async def test_activate_user_success_with_new_code(
+    async_client: AsyncClient,
+) -> None:
+    payload = {
+        "email": TEST_EMAIL,
+        "password": TEST_PASSWORD,
+        "code": TEST_VERIFICATION_CODE_NEW,
+    }
+    response = await async_client.post("/v1/users/activate", json=payload)
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert "Account activated successfully" in data["message"]
